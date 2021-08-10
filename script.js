a231d39820a220ec4b9ad39bd8fd281468861e58
HeilerOs <88403843+HeilerOs@users.noreply.github.com>
2021-08-10 02:21:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-function compute() {
- var principal = document.getElementById("principal").value;
-
- if (principal == "" || principal <= 0) {
-  alert("Enter a positive number");
-  document.getElementById("principal").focus();
-  return;
- }
-
- var rate = document.getElementById("rate").value;
- var years = document.getElementById("years").value;
- var interest = (principal * years * rate) / 100;
-
- var dateNow = new Date();
- var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
-
- var resultDisplay = document.getElementById("result");
- resultDisplay.innerHTML =
-  "If you deposit " +
-  "<span class='highlight'>" +
-  principal +
-  "</span>." +
-  ", <br> at an interest rate of " +
-  "<span class='highlight'>" +
-  rate +
-  "</span>%." +
-  "<br> You will receive an amount of " +
-  "<span class='highlight'>" +
-  interest +
-  "</span>" +
-  ", <br> in the year " +
-  "<span class='highlight'>" +
-  yearNow +
-  "</span>";
+function func1() {
+    var principal = document.getElementById("principal").value;
+    var interes = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * interes /100) ;
+    var year1 = new Date().getFullYear()+parseInt(years);
+    var texto = `if you deposit ${principal} at interest rate of ${interes} % you will recive an amount of ${interest}, in the year ${year1}`
+   
+    document.getElementById("interes").innerText= texto;
 }
-
-
-function updateRate() {
- var rateval = document.getElementById("rate").value;
- document.getElementById("rate_val").innerText = rateval;
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
